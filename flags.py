5fbdd1c898b9ea8532168193ca5fc65f343d6038
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2022-02-05 18:05:10 +0800

    update flags.py to avoid too many screen writes

--- flags.py
+++ flags.py
@@ -1,4 +1,5 @@
 from micropython import opt_level
+from pyb import wfi
 
 opt_level(3)
 
@@ -16,13 +17,20 @@ class FlagsScript:
         self.led2 = led2
 
     def japan(self):
-        self.screen.fill((255, 255, 255))
-        self.screen.circle(80, 64, 40, (255, 0, 0), (0, 255, 0))
+        self.screen.fill((255, 255, 255), update=False)
+        self.screen.circle(80, 64, 40, (255, 0, 0), (0, 255, 0), update=False)
+        self.screen.refresh()
 
     def france(self):
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((255, 255, 255), update=False)
         self.screen.rect(
-            0, 0, self.screen_width // 3, self.screen_height, (0, 0, 255), (0, 0, 255)
+            0,
+            0,
+            self.screen_width // 3,
+            self.screen_height,
+            (0, 0, 255),
+            (0, 0, 255),
+            update=False,
         )
         self.screen.rect(
             (self.screen_width // 3) * 2,
@@ -31,12 +39,20 @@ class FlagsScript:
             self.screen_height,
             (255, 0, 0),
             (255, 0, 0),
+            update=False,
         )
+        self.screen.refresh()
 
     def italy(self):
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((255, 255, 255), update=False)
         self.screen.rect(
-            0, 0, self.screen_width // 3, self.screen_height, (0, 255, 0), (0, 255, 0)
+            0,
+            0,
+            self.screen_width // 3,
+            self.screen_height,
+            (0, 255, 0),
+            (0, 255, 0),
+            update=False,
         )
         self.screen.rect(
             (self.screen_width // 3) * 2,
@@ -45,7 +61,9 @@ class FlagsScript:
             self.screen_height,
             (255, 0, 0),
             (255, 0, 0),
+            update=False,
         )
+        self.screen.refresh()
 
     # main function
     def main(self):
@@ -53,6 +71,7 @@ class FlagsScript:
         self.flag_index = 0
         self.flags_list[self.flag_index]()
         while True:
+            wfi()
             if self.sensor.btnValue("right") == 1:
                 self.flag_index = (self.flag_index + 1) % len(self.flags_list)
                 self.flags_list[self.flag_index]()
